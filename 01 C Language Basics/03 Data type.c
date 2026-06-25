eae3257b741810410d5f54d34b0e200d60fb3bde
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-25 15:12:44 +0530

    Create 03 Data type.c

---
+++ 01 C Language Basics/03 Data type.c
@@ -0,0 +1,20 @@
+//Everything is called data.
+//There are Many types of Data:-
+#include <stdio.h>
+int main()
+{
+    int age=20;
+    float percentage=85.5;
+    char grade='A';
+    double salary = 50000.75;
+    printf("Age=%d\n",age);
+    printf("Percentage=%.2f\n", percentage);
+    printf("Grade=%c\n",grade);
+    printf("Salary = %.2lf\n", salary);
+    return 0;
+}
+//int → Integer values
+//float → Decimal values
+//char → single character
+//double → Large decimal values
+//string is not possible in c language without array.
