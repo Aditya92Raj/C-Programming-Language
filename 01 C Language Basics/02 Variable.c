c778b3d6a5bf8a045b8d866e6c9467dbd43e130f
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-25 14:47:13 +0530

    Create 02 Variable.c

---
+++ 01 C Language Basics/02 Variable.c
@@ -0,0 +1,14 @@
+//Variable is a container that stores various types of data in program.
+#include<stdio.h>
+int main()
+{
+    char Name='A';
+    int Age=20;
+    printf("Name in character form=%c\n",Name);
+    printf("Age=%d",Age);
+}
+//Variable can not be starts from number.so "1Name" is not true but "Name1" is always true.
+
+//space is not include in variable name.so "Na me" is not true but "Name" is true.
+
+//we cannot use special symbol or inverted commas for variable name.so "Name@12" is not true.
