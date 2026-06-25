a0543c5ef2a306ab8daac3813e7b4e63259709b3
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-25 15:20:50 +0530

    Create 04 Line Breaking.c

---
+++ 01 C Language Basics/04 Line Breaking.c
@@ -0,0 +1,7 @@
+//Line Break in Python.
+#include<stdio.h>
+int main()
+{
+    printf("My name is Aditya Raj.\nI am from Vaishali District.\nCurrently I am pursuing BCA from OCRM Patna, Bihar.");
+}
+//Any data or values cannot start from new line automatically in C. It is only possible with the help of line break ("\n").
