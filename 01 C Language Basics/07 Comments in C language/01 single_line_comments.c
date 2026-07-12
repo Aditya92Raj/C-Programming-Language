7afaeb4c50949aa65682f35672a08a5684dc120a
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-12 23:52:01 +0530

    Create 01 single_line_comments.c

---
+++ 01 C Language Basics/07 Comments in C language/01 single_line_comments.c
@@ -0,0 +1,19 @@
+//Comments in python:-
+//Single line comments with the help of forward slash(//).
+#include<stdio.h>
+int main()
+{
+    int a=68, b=40, Sum, difference, multiply;
+    float divide;
+    Sum=a+b;
+    difference=a-b;
+    multiply=a*b;
+    divide=a/b;
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    printf("Sum=%d\n",Sum);
+    printf("difference=%d\n",difference);
+    //printf("multiply=%d\n",multiply);
+    //printf("divide=%f\n",divide);
+return 0;
+}
