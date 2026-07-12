b1ff8fff50c9237592aef24af204014e56412dd7
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-12 23:45:33 +0530

    Create 06 Basic_program.c

---
+++ 01 C Language Basics/06 Basic_program.c
@@ -0,0 +1,18 @@
+//Basic program in python.
+#include<stdio.h>
+int main()
+{
+    int a=50, b=25, Sum, difference, multiply;
+    float divide;
+    Sum=a+b;
+    difference=a-b;
+    multiply=a*b;
+    divide=a/b;
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    printf("Sum=%d\n",Sum);
+    printf("difference=%d\n",difference);
+    printf("multiply=%d\n",multiply);
+    printf("divide=%f\n",divide);
+return 0;
+}
