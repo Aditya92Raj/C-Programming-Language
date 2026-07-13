00081cbb3b41c99803afa70ab00e2d03d62fdff2
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-13 16:59:46 +0530

    Create 05 calculate_area_of_rectange.c

---
+++ 01 C Language Basics/09 Input & Output/05 calculate_area_of_rectange.c
@@ -0,0 +1,12 @@
+//Write a program to input length & breadth of rectangle from user and calculate area of rectangle.
+#include <stdio.h>
+int main()
+{
+    int l,b;
+    printf("Enter a length:");
+    scanf("%d",&l);
+    printf("Enter a breadth:");
+    scanf("%d",&b);
+    printf("area of rectangle=%d",l*b);
+return 0;
+}
