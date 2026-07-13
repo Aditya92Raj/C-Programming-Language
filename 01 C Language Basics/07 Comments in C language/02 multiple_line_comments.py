c295c6da9cf69614a8d48dadf1315aead7ffcd4e
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-13 15:39:04 +0530

    Create 02 multiple_line_comments.py

---
+++ 01 C Language Basics/07 Comments in C language/02 multiple_line_comments.py
@@ -0,0 +1,19 @@
+//Comments in python:-
+//Multiple line comments with the help of forward slash & star(/*).
+#include<stdio.h>
+int main()
+{
+    int a=48, b=20, Sum, difference, multiply;
+    float divide;
+    Sum=a+b;
+    difference=a-b;
+    multiply=a*b;
+    divide=a/b;
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    printf("Sum=%d\n",Sum);
+    printf("difference=%d\n",difference);
+    /*printf("multiply=%d\n",multiply);
+    printf("divide=%f\n",divide);*/
+return 0;
+}
