4db4822362a446b9e3a7e745c4e50f96c42935fc
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-13 16:55:46 +0530

    Create 04 calculate_area_of_square.c

---
+++ 01 C Language Basics/09 Input & Output/04 calculate_area_of_square.c
@@ -0,0 +1,10 @@
+//Write a program to input side of square from user and calculate area of square.
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a side:");
+    scanf("%d",&a);
+    printf("area of square=%d",a*a);
+return 0;
+}
