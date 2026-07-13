0d6c6f51adfd315a04dce34641dbb40c238c1d6c
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-13 15:49:42 +0530

    Create 01 implicit.c

---
+++ 01 C Language Basics/08 Type-Casting/01 implicit.c
@@ -0,0 +1,11 @@
+//Type-Casting or Type-Conversion:-
+//Implicit Type Conversion (Automatic)
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    float b = a;
+    printf("%f", b);
+    return 0;
+}
+//The compiler automatically converts one data type to another.
