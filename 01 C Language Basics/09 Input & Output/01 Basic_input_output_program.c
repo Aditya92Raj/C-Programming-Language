48c46c643679b4c24831e897b87315185c7efbbe
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-13 16:18:07 +0530

    Create 01 Basic_input_output_program.c

---
+++ 01 C Language Basics/09 Input & Output/01 Basic_input_output_program.c
@@ -0,0 +1,14 @@
+//Basic input & output program.
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter your first number:");
+    scanf("%d",&a);
+    printf("Enter your second number:");
+    scanf("%d",&b);
+    printf("Addition is %d\n",a+b);
+    printf("subtraction is %d\n",a-b);
+    printf("multiplication is %d\n",a*b);
+    return 0;
+}
