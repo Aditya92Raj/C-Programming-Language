f5cac88f923bf7cdebb0659e6664aa7952ce55bd
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-13 15:53:51 +0530

    Create 02 explicit.c

---
+++ 01 C Language Basics/08 Type-Casting/02 explicit.c
@@ -0,0 +1,10 @@
+//Type-Casting or Type-Conversion:-
+//Explicit Type Conversion
+#include <stdio.h>
+int main()
+{
+    float a = 99.99;
+    printf("%d", (int)a);
+    return 0;
+}
+//The programmer manually converts the data type using the cast operator.
