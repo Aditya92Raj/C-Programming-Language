cfa63cf25fd951f707619f25b049d6a3b122cbde
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-14 16:38:56 +0530

    Create 06 calculate_volume_of_cube.c

---
+++ 01 C Language Basics/09 Input & Output/06 calculate_volume_of_cube.c
@@ -0,0 +1,9 @@
+//Write a program to input side of cube from user and calculate volume of cube.
+#include <stdio.h>
+int main() {
+    int a;
+    printf("Enter a side of cube:");
+    scanf("%d",&a);
+    printf("Volume of cube=%d",a*a*a);
+    return 0;
+}
