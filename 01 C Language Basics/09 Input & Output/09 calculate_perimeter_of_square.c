ee80952393512e0eadb3e52bbedf318829549fd1
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-14 21:26:52 +0530

    Create 09 calculate_perimeter_of_square.c

---
+++ 01 C Language Basics/09 Input & Output/09 calculate_perimeter_of_square.c
@@ -0,0 +1,9 @@
+//Write a program to input side of square and calculate perimeter of square.
+#include <stdio.h>
+int main() {
+    int a;
+    printf("Enter a side of square:");
+    scanf("%d",&a);
+    printf("Perimeter of square=%d",4*a);
+    return 0;
+}
