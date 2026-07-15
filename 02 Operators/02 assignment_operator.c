1eec98442403bccd24029c9fd9fbe5d314e95489
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-15 13:59:07 +0530

    Create 02 assignment_operator.c

---
+++ 02 Operators/02 assignment_operator.c
@@ -0,0 +1,16 @@
+//Operator in C language:-
+//Assignment Operator(+=,-=,*=,/=,%=)
+#include <stdio.h>
+int main() {
+    int a=5,b=22;
+    printf("before assignment\n");
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    a+=15;
+    b-=10;
+    printf("after assignment\n");
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    return 0;
+}
+//assignment operator is used to assing value and change the value.
