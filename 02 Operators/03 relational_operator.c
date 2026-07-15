b096830583a95ed59f009ab7c5cce01b742ef786
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-15 14:15:27 +0530

    Create 03 relational_operator.c

---
+++ 02 Operators/03 relational_operator.c
@@ -0,0 +1,13 @@
+//Operator in C language:-
+//Relational Operator(==,!=,<=,>=,<,>)
+#include <stdio.h>
+int main() {
+    int a=5,b=22;
+    printf("%d\n",a>=b);
+    printf("%d\n",a<=b);
+    printf("%d\n",a<b);
+    printf("%d\n",a>b);
+    return 0;
+}
+//relational operator is used to compare the value and result or output is always present as Boolean (0 and 1).
+//If condition is true output will 1 and if condition is false output will 0.
