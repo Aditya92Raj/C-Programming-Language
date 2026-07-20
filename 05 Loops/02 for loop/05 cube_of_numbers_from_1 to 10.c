94fbc4359e91bc149cc98160534e24199a954966
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-20 23:49:56 +0530

    Create 05 cube_of_numbers_from_1 to 10.c

---
+++ 05 Loops/02 for loop/05 cube_of_numbers_from_1 to 10.c
@@ -0,0 +1,10 @@
+//write a program to print cube of numbers from 1 to 10 using for loop in c language.
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        printf("%d\n", i*i*i);
+    }
+    return 0;
+}
