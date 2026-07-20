fe9376d8f648f33f25adc4337637ab9a1c771401
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-20 23:45:54 +0530

    Create 03 print_numbers_from_50 to 1.c

---
+++ 05 Loops/02 for loop/03 print_numbers_from_50 to 1.c
@@ -0,0 +1,10 @@
+//write a program to print numbers in reverse order from 50 to 1 using for loop in c language.
+#include <stdio.h>
+int main()
+{
+    for(int i=50;i>=1;i--)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
