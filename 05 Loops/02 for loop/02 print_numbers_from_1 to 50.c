e8baae9f101b1a91687f4f09e29eee3c04f65d74
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-20 23:43:51 +0530

    Create 02 print_numbers_from_1 to 50.c

---
+++ 05 Loops/02 for loop/02 print_numbers_from_1 to 50.c
@@ -0,0 +1,10 @@
+//write a program to print numbers from 1 to 50 using for loop in c language.
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=50;i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
