aa59a4f5a5d496d738497d1c93ff61bae0435d84
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-20 21:51:44 +0530

    Create 01 Basic_loop_program.c

---
+++ 05 Loops/01 Basic_loop_program.c
@@ -0,0 +1,18 @@
+//A loop is a programming statement that allows a block of code to execute repeatedly until a specified condition becomes false. Loops help avoid writing the same code multiple times.
+
+//there are three types of loops:-
+//(i)for loop
+//(ii)while loop
+//(iii)do-while loop
+
+#include <stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=5)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
