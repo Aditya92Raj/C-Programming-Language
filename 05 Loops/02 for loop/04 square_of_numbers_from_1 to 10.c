b297852d966194e294709edc03186710efab298d
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-20 23:48:43 +0530

    Create 04 square_of_numbers_from_1 to 10.c

---
+++ 05 Loops/02 for loop/04 square_of_numbers_from_1 to 10.c
@@ -0,0 +1,10 @@
+//write a program to print square of numbers from 1 to 10 using for loop in c language.
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        printf("%d\n", i*i);
+    }
+    return 0;
+}
