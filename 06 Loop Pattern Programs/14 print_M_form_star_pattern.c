7efd453cd18fb525425d0bbf05ec8c743956ee87
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-08-05 23:37:53 +0530

    Create 14 print_M_form_star_pattern.c

---
+++ 06 Loop Pattern Programs/14 print_M_form_star_pattern.c
@@ -0,0 +1,23 @@
+//Write a program to print M form star pattern in c language.
+#include <stdio.h>
+int main() {
+    int a,i,j;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        for(j=1;j<=a;j++)
+        {
+            if(j==1 || j==a || i==j && i<=a/2+1 || a+1-i==j && i<=a/2+1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
