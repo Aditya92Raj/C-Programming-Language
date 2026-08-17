7c8d79acb2bebd7f7bb0fbcbdd37e82972c8de92
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-08-17 17:16:08 +0530

    Update 01 function_first_program.c

--- 07 Functions/01 function_first_program.c
+++ 07 Functions/01 function_first_program.c
@@ -1,3 +1,6 @@
+//A function in C is a block of code that performs a specific task. Functions help us divide a large program into smaller, reusable parts.
+
+//Function with no arguments and no return.
 #include<stdio.h>
 void greet()
 {
