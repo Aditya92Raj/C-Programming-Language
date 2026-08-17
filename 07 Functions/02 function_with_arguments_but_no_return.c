55111de6353be97ef37e126367396c8ef4e367db
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-08-17 17:20:41 +0530

    Create 02 function_with_arguments_but_no_return.c

---
+++ 07 Functions/02 function_with_arguments_but_no_return.c
@@ -0,0 +1,12 @@
+//Function with arguments but no any return value.
+#include <stdio.h>
+void add(int a, int b)
+{
+    int sum=a+b;
+    printf("Sum=%d",sum);
+}
+int main()
+{
+    add(10,20);
+    return 0;
+}
