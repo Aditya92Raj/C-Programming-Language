abc43ccef44ebbbdf0f3c3dd0850093971c27f45
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-12 15:28:52 +0530

    Create 01 pre-increment operator.c

---
+++ 02 Operators/05 Increment operator/01 pre-increment operator.cpp
@@ -0,0 +1,10 @@
+//Operator in C language:-
+//pre-increment operator(++a)
+#include <stdio.h>
+int main() {
+    int a=5;
+    printf("%d\n",++a);
+    printf("%d\n",a);
+return 0;
+}
+//The pre-increment operator increases the value of a variable by 1 before using its value in an expression.
