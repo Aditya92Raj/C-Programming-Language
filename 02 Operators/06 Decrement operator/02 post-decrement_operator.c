b987771a0a2c5848e4ce786298de9e14cd446005
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-12 15:50:16 +0530

    Create 02 post-decrement_operator.c

---
+++ 02 Operators/06 Decrement operator/02 post-decrement_operator.c
@@ -0,0 +1,10 @@
+//Operator in C language:-
+//post-decrement operator(a--)
+#include <stdio.h>
+int main() {
+    int a=5;
+    printf("%d\n",a--);
+    printf("%d\n",a);
+return 0;
+}
+//The post-decrement operator uses the current value of a variable first and then increase it by 1.
