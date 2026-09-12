28862c0d1c98bd41b86df60ca9d42a7bcf41dea3
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-12 15:32:40 +0530

    Create 01 pre-decrement operator.c

---
+++ 02 Operators/06 Decrement operator/01 pre-decrement operator.c
@@ -0,0 +1,10 @@
+//Operator in C language:-
+//pre-decrement operator(--a)
+#include <stdio.h>
+int main() {
+    int a=5;
+    printf("%d\n",--a);
+    printf("%d\n",a);
+return 0;
+}
+//The pre-decrement operator decrease the value of a variable by 1 before using its value in an expression.
