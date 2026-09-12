11c3e395a3f1f8f71f2349e57f7f71a04eb4f3dc
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-12 15:48:34 +0530

    Create 02 post-increment_operator.c

---
+++ 02 Operators/05 Increment operator/02 post-increment_operator.c
@@ -0,0 +1,10 @@
+//Operator in C language:-
+//post-increment operator(a++)
+#include <stdio.h>
+int main() {
+    int a=5;
+    printf("%d\n",a++);
+    printf("%d\n",a);
+return 0;
+}
+//The post-increment operator uses the current value of a variable first and then increase it by 1.
