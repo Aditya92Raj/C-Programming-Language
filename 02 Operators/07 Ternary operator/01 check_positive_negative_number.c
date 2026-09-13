d12fd9dd6ede5406b1e561a80c9d99b59f2cf9cb
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-13 14:05:01 +0530

    Create 01 check_positive_negative_number.c

---
+++ 02 Operators/07 Ternary operator/01 check_positive_negative_number.c
@@ -0,0 +1,10 @@
+//Operator in C language:-
+//Ternary Operator (condition?exp1:exp2;)
+#include <stdio.h>
+int main() {
+    int x;
+    printf("Enter a number:");
+    scanf("%d",&x);
+    (x>=0)?printf("positive number"):printf("negative number");
+    return 0;
+}
