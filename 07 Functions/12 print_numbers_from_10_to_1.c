5913408dd444d426016f9880941b0ccba8885605
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-14 23:24:39 +0530

    Create 12 print_numbers_from_10_to_1.c

---
+++ 07 Functions/12 print_numbers_from_10_to_1.c
@@ -0,0 +1,13 @@
+//Write a function to print numbers from 10 to 1.
+#include <stdio.h>
+void num()
+{
+    for(int i=10;i>=1;i--)
+        {
+            printf("%d\n",i);
+        }
+}
+int main() {
+    num();
+    return 0;
+}
