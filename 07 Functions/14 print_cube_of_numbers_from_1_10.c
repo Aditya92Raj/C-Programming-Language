4867dd61d57feaa849ca67561e403bba7660c9f1
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-14 23:30:28 +0530

    Create 14 print_cube_of_numbers_from_1_10.c

---
+++ 07 Functions/14 print_cube_of_numbers_from_1_10.c
@@ -0,0 +1,13 @@
+//Write a function to print cube of numbers from 1 to 10.
+#include <stdio.h>
+void num()
+{
+    for(int i=1;i<=10;i++)
+        {
+            printf("%d\n",i*i*i);
+        }
+}
+int main() {
+    num();
+    return 0;
+}
