4e0bbb412750856b86c923548a0b48221c4c8bfc
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-23 23:39:38 +0530

    Create 08 sizeof()_operator.c

---
+++ 02 Operators/08 sizeof()_operator.c
@@ -0,0 +1,8 @@
+//sizeof is a unary operator in C that is used to find the size of a data type, variable, array, structure, etc. in bytes.
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("%d",sizeof(a));
+    return 0;
+}
