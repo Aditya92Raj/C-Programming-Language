9aa4e8b3650110ee00fcfd4e752ae4971a6d8410
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-24 23:12:03 +0530

    Create 03 Bitwise_XOR(^)_operator.c

---
+++ 02 Operators/10 Bitwise_Operators/03 Bitwise_XOR(^)_operator.c
@@ -0,0 +1,9 @@
+//The Bitwise XOR (Exclusive OR) operator (^) compares the corresponding bits of two integer operands.
+#include <stdio.h>
+int main()
+{
+    int a=5,b=3;
+    printf("%d",a^b);
+    return 0;
+}
+//The result is 1 when the two bits are different and 0 when they are the same.
